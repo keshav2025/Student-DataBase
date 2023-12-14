7302a590617a4adcbacdee6ba1e392d95cc3b6ef
keshav2025 <keshavgupta86036@gmail.com>
2023-12-14 15:26:42 +0530

    first commit

---
+++ Test.java
@@ -0,0 +1,40 @@
+public class Test {
+//     static class Solution {
+//         public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+//             String str1 = "";
+//             String str2 = "";
+//             for (String s : word1) {
+//                 str1 += s;
+//             }
+//             for (String s : word2) {
+//                 str2 += s;
+//             }
+//             return str1.equals(str2);
+//         }
+//     }
+
+    public static void main(String[] args) {
+//         Solution solution = new Solution();
+// String [] word1 = {"ab","c"};
+// String [] word2 = {"ab","c"};
+//               String str1 =" ";
+//               String str2 =" ";
+//               for(int i =0; i<word1.length; i++){
+//                   str1+=word1[i];
+//               }
+//               for(int i =0; i<word2.length; i++){
+//                  str2=word2[i];
+//               }
+//               System.out.println(str1.equals(str2));
+             
+           
+              
+      
+          }
+      }
+//        
+
+//         boolean result = solution.arrayStringsAreEqual(word1, word2);
+//         System.out.println("Are the concatenated strings equal? " + result);
+   // }
+    
